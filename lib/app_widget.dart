c3c7ca02940751ab692cd206f07554e3c05c5d88
gvheisler <gvheisler@inf.ufsm.br>
2022-12-29 13:47:38 -0300

    update

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 
@@ -6,6 +7,65 @@ class AppWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return CupertinoApp(
+      home: CupertinoPageScaffold(
+        backgroundColor: Colors.lightBlueAccent,
+        navigationBar: const CupertinoNavigationBar(
+          middle: Text('Tecnologia em saúde'),
+          backgroundColor: Colors.lightBlue,
+        ),
+        child: Center(
+          child: SizedBox(
+            height: double.infinity,
+            width: 350,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+
+                CupertinoTextField(
+                    placeholder: "Usuário",
+                    placeholderStyle: TextStyle(color: Colors.grey),
+                    textAlign: TextAlign.left,
+                    style: TextStyle(color: Colors.black),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                        width: 3,
+                      ),
+                      borderRadius: BorderRadius.circular(12),
+
+                    )
+                ),
+                SizedBox(height: 20,),
+                CupertinoTextField(
+                    placeholder: "Senha",
+                    placeholderStyle: TextStyle(color: Colors.grey),
+                    textAlign: TextAlign.left,
+                    style: TextStyle(color: Colors.black),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                        width: 3,
+                      ),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    obscureText: true,
+                ),
+                SizedBox(height: 20,),
+                CupertinoButton(
+                    color: Colors.blue[800],
+                    child: Text("Autenticar"),
+                    onPressed: (){
+
+                    },
+                    borderRadius: BorderRadius.circular(24),
+                )
+              ],
+            ),
+          )
+      ),
+
+      ),
+    );
   }
 }
